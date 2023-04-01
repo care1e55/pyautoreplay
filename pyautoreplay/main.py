b7ca28bce265cf30fe894033e0a15b0686663ec7
care1e55 <sfilsm89@gmail.com>
2023-04-01 20:02:23 +0300

    add: error base

--- pyautoreplay/main.py
+++ pyautoreplay/main.py
@@ -3,20 +3,32 @@ from pathlib import Path
 from loguru import logger
 
 from pyautoreplay.storage.storage import ReplayStorage
-from pyautoreplay.watcher.legacy import ReplayWatcher, System
+from pyautoreplay.watcher.legacy import ReplayWatcher, System, ReplayError, Action
 
 if __name__ == '__main__':
     storage_path = Path(sys.argv[1])
     watching_path = Path(sys.argv[2])
     logger.info('Starting replays from {}', storage_path)
-    replays_storage_path = Path(storage_path)
-    rs = ReplayStorage(replays_storage_path)
+    rs = ReplayStorage(storage_path)
     rw = ReplayWatcher(rs, watching_path, system=System.WINDOWS)
 
     for i, replay in enumerate(rs.replays()):
-        rw._clean_watching_dir()
-        rw._move_to_watcing_dir(replay)
-        rw.init_games_screen()
-        rw.init_replay()
-        rw.watch(replay)
-        rw.exit_replay()
+        rw.clean_watching_dir().move_to_watcing_dir(replay).init_games_screen().start_replay()
+        if not rw.error:
+            rw.configure_replay()\
+                .watch(replay)\
+                .exit_replay()\
+                .do_action(Action.CANCEL)\
+                .do_action(Action.CANCEL)
+            continue
+        if rw.error == ReplayError.EXPANSION_SCENARIO:
+            rw\
+                .do_action(Action.OK)\
+                .do_action(Action.CANCEL)\
+                .do_action(Action.CANCEL)\
+                .do_action(Action.CANCEL)
+        elif rw.error == ReplayError.FATAL:
+            # reboot app
+            pass
+        else:
+            raise 'Unknown error'
--- pyautoreplay/watcher/legacy.py
+++ pyautoreplay/watcher/legacy.py
@@ -1,6 +1,8 @@
 import time
 from enum import Enum
+from functools import cached_property
 from pathlib import Path
+from typing import Optional
 
 import pyautogui
 from tqdm import tqdm
@@ -8,6 +10,11 @@ from loguru import logger
 
 from pyautoreplay.replay.screp.screp import Replay
 from pyautoreplay.storage.storage import ReplayStorage
+
+
+class ReplayError(Enum):
+    EXPANSION_SCENARIO = 'expansion'
+    FATAL = 'fatal'
 
 
 class Action(str, Enum):
@@ -48,13 +55,12 @@ class ReplayWatcher:
             raise ValueError('No such window manager')
 
     def watch(self, replay: Replay):
-        # self.init_replay()
-        # print(f'Watching replay {replay.name} for {replay.duration} seconds')
+        """Sleep for duration"""
         for _ in tqdm(range(replay.duration), desc=f'Watching replay {replay.name} for {replay.duration} seconds'):
             time.sleep(1)
         self.exit_replay()
 
-    def _do_action(self, key: str, delay: float = 0.2):
+    def do_action(self, key: str, delay: float = 0.2):
         window = self.window_manager.find_window(self.WINDOW)
         time.sleep(delay)
         self._press_key(key)
@@ -66,30 +72,38 @@ class ReplayWatcher:
         time.sleep(delay)
 
     def init_games_screen(self):
-        self._do_action(Action.SINGLE_PLAYER)
-        self._do_action(Action.EXPANSION)
-        self._do_action(Action.OK)
+        self.do_action(Action.SINGLE_PLAYER)
+        self.do_action(Action.EXPANSION)
+        self.do_action(Action.OK)
+        return self
 
-    def init_replay(self):
+    def start_replay(self):
         time.sleep(3)
-        self._do_action(Action.REPLAY)
-        self._do_action(Action.DOWN)
-        self._do_action(Action.OK)
-        self._do_action(Action.SPEEDUP)
-        self._do_action(Action.SPEEDUP)
-        self._do_action(Action.SWITCH_PLAYER)
-        self._do_action(Action.SWITCH_PLAYER)
+        self.do_action(Action.REPLAY)
+        self.do_action(Action.DOWN)
+        self.do_action(Action.OK)
+        return self
+
+    def configure_replay(self):
+        self.do_action(Action.SPEEDUP)
+        self.do_action(Action.SPEEDUP)
+        self.do_action(Action.SWITCH_PLAYER)
+        self.do_action(Action.SWITCH_PLAYER)
+        return self
 
     def exit_replay(self):
-        self._do_action(Action.EXIT)
+        self.do_action(Action.EXIT)
         time.sleep(3)
-        self._do_action(Action.OK)
-        self._do_action(Action.CANCEL)
-        self._do_action(Action.CANCEL)
+        self.do_action(Action.OK)
+        return self
 
-    def _move_to_watcing_dir(self, replay: Replay):
+    @cached_property
+    def error(self) -> Optional[ReplayError]:
+        return None
+
+    def move_to_watcing_dir(self, replay: Replay):
         replay.copy(self.watching_path / replay.path.name)
 
-    def _clean_watching_dir(self):
+    def clean_watching_dir(self):
         for replay in self.watching_path.glob('*.rep'):
             replay.unlink()
